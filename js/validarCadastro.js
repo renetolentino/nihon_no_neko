586a0cc1b78b6ec4ad6902bb9e98b32d786d3c8d
Renê Tolentino <114789737+renetolentino@users.noreply.github.com>
2024-06-02 13:34:19 -0300

    Bug fixs
    Some bugs fixed, new bugs added :D

--- js/validarCadastro.js
+++ js/validarCadastro.js
@@ -7,21 +7,42 @@ class Usuario {
     this.passConfirm = passConfirm;
     this.contrato = contrato;
     this.politica = politica;
+    this.idade;
+    this.verificado = false;
   }
 
-  validar() {
+  calcIdade() {
     nascimento = this.nascimento.split('-');
     const novaData = [nascimento[1], nascimento[2], nascimento[0]];
     this.nascimento = new Date(novaData.join('-'));
     const idade = Math.floor((Date.now() - this.nascimento) / 31556952000);
+    return (this.idade = idade);
+  }
 
-    // alert('Estamos validando!');
-    if (!(this.pass === this.passConfirm)) alert('Senhas não conferem');
-    if (idade < 18) alert('Fique mais velho primeiro!');
-    if (!this.contrato) alert('Leia e concorde com o nosso contrato!');
-    if (!this.politica) alert('Leia e concorde com nossas políticas de uso!');
-    if (this.email == /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i)
+  validar() {
+    this.calcIdade();
+    // console.log(this.pass);
+    if (!this.pass || !this.passConfirm || !(this.pass === this.passConfirm)) {
+      alert('Senhas não conferem');
+      return false;
+    }
+    if (this.idade < 18) {
+      alert('Fique mais velho primeiro!');
+      return false;
+    }
+    if (!this.contrato) {
+      alert('Leia e concorde com o nosso contrato!');
+      return false;
+    }
+    if (!this.politica) {
+      alert('Leia e concorde com nossas políticas de uso!');
+      return false;
+    }
+    if (this.email == /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i) {
       alert('Digite um email válido!');
+      return false;
+    }
+    return true;
   }
 }
 
@@ -33,9 +54,9 @@ const registrar = () => {
   const passConfirm = document.querySelector('#passConfirm').value;
   const contrato = document.querySelector('#contrato').checked;
   const politicas = document.querySelector('#politicas').checked;
-  console.log(
-    `${nome}\n${email}\n${nascimento}\n${pass}\n${passConfirm}\nContrato: ${contrato}\nPolíticas: ${politicas}`
-  );
+  // console.log(
+  //   `${nome}\n${email}\n${nascimento}\n${pass}\n${passConfirm}\nContrato: ${contrato}\nPolíticas: ${politicas}`
+  // );
 
   const usuario = new Usuario(
     nome,
@@ -46,5 +67,48 @@ const registrar = () => {
     contrato,
     politicas
   );
-  usuario.validar();
+  if (usuario.validar()) bemVindo(usuario);
+};
+
+const bemVindo = (user) => {
+  console.log(document.querySelector('#cadastro'));
+  const welcomido = `
+  <div class='header'>
+    <header>
+      <h2>Seja bem-vindo!</h2>
+    </header>
+  </div>
+  <div class='row'>
+    <div class='column center'>
+      <h2>Perfil</h2>
+    </div>
+  </div>
+  <div class='row center'>
+    <div class='foto'></div>
+  </div>
+  <div class='row'>
+    <div class='column'>
+      <h3>Nome:${user.nome}</h3> 
+    </div>
+  </div>
+  <div class='row'>
+    <div class='column'>
+      <h3>Idade:${user.idade}</h3> 
+    </div>
+  </div>
+  <div class='row'>
+    <div class='column'>
+      <h3>E-mail:${user.email}</h3> 
+    </div>
+  </div>
+  <div class='row'>
+    <div class='column'>
+      <h3>E-mail verificado:${user.verificado ? 'Sim ✅' : 'Não ❌'}</h3> 
+    </div>
+  </div>
+  <div class='row form-group center'>
+    <button>Voltar</button>
+  </div>
+  `;
+  document.querySelector('#cadastro').innerHTML = welcomido;
 };
